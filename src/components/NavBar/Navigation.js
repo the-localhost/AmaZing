09a583e9aebf6cbe6a2d676fd0eb687f3a0bd5b9
Gagan Agrawal <gaganagrawal.iit@gmail.com>
2023-04-24 18:49:16 +0530

    added searchbox in Navbar

--- src/components/NavBar/Navigation.js
+++ src/components/NavBar/Navigation.js
@@ -15,6 +15,13 @@ class NavBar extends Component {
                     <div className="navbar__locatorImage"></div>
                     <div className="navbar__location">Pune</div>
                 </div>
+                <div className="navbar__search">
+                    <div></div>
+                    <div>
+                        <input type="text" className="navbar__searchBox"></input>
+                    </div>
+                    <div></div>
+                </div>
             </div>
          );
     }
