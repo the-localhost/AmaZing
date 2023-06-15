aa348c4d2773a5c27f1572cddec6e014ffaa163a
Gagan Agrawal <gaganagrawal.iit@gmail.com>
2023-06-15 22:54:23 +0530

    created main page

---
+++ src/components/MainPage/MainPage.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+import './MainPage.css'
+
+class MainPage extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    
+    render() { 
+        return ( 
+            <div className="mainpage">
+                MainPage
+            </div>
+         );
+    }
+}
+ 
+export default MainPage;   
