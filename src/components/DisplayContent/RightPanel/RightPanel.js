03f1386c55b06e493959d0efda4df1427ddef7a5
Gagan Agrawal <gaganagrawal.iit@gmail.com>
2023-06-16 14:23:45 +0530

    Created left and right panel to display products and its options

---
+++ src/components/DisplayContent/RightPanel/RightPanel.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './RightPanel.css'
+
+function RightPanel() {
+  return (
+    <div className="rightSide__main">RightPanel</div>
+  )
+}
+
+export default RightPanel
