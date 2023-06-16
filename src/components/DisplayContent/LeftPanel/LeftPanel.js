81e14a019a5764442f7241a1717d27f10b5d5812
Gagan Agrawal <gaganagrawal.iit@gmail.com>
2023-06-16 14:31:02 +0530

    Created loop for brands

--- src/components/DisplayContent/LeftPanel/LeftPanel.js
+++ src/components/DisplayContent/LeftPanel/LeftPanel.js
@@ -2,13 +2,19 @@ import React from 'react'
 import './LeftPanel.css'
 
 function LeftPanel() {
+    const brands = ["Apple", "Samsung", "Mi", "Oppo"]
   return (
     <div className="leftSide__main">
         <div className="leftSide__header">
             Brand
         </div>
         <div className="leftSide__brandNames">
-            <label className="brandName">
+            {brands.map((brand) => (
+                <label className="brandName">
+                    <input type="checkbox" value={brand} /> {brand}
+                </label>)
+                )}
+            {/* <label className="brandName">
                 <input type="checkbox" value="Apple" /> Apple
             </label>
             <label className="brandName">
@@ -19,7 +25,7 @@ function LeftPanel() {
             </label>
             <label className="brandName">
                 <input type="checkbox" value="Oppo" /> Oppo
-            </label>
+            </label> */}
         </div>
     </div>
   )
