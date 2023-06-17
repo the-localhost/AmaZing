441e7f7f50a957e7d1dcca389aeaad8ae77e1d46
Gagan Agrawal <gaganagrawal.iit@gmail.com>
2023-06-17 15:37:53 +0530

    wrapping elements with CartContextProvider

--- src/App.js
+++ src/App.js
@@ -5,17 +5,20 @@ import MainPage from './components/MainPage/MainPage';
 import NavBar from './components/NavBar/Navigation';
 import PlaceOrder from './components/PlaceOrder/PlaceOrder';
 import { Routes, Route } from "react-router-dom";
+import CartContextProvider from './components/CartContext';
 
 function App() {
   return (
     <div className="App">
-      <NavBar />
-      <Routes>
-          <Route path="/display" element={<DisplayContent/>} />
-          <Route path="/order/:id" element={<PlaceOrder />} />
-          <Route path="/checkout" element={<Checkout />} />
-          <Route path="/" element={<MainPage />} />
-      </Routes>
+      <CartContextProvider >
+        <NavBar />
+        <Routes>
+            <Route path="/display" element={<DisplayContent/>} />
+            <Route path="/order/:id" element={<PlaceOrder />} />
+            <Route path="/checkout" element={<Checkout />} />
+            <Route path="/" element={<MainPage />} />
+        </Routes>
+      </CartContextProvider>
     </div>
   );
 }
