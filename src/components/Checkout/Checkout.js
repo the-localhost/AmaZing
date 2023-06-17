321bf62aea796e6021be37301103370502c50d20
Gagan Agrawal <gaganagrawal.iit@gmail.com>
2023-06-17 16:18:13 +0530

    dynamic values on checkout page

--- src/components/Checkout/Checkout.js
+++ src/components/Checkout/Checkout.js
@@ -1,9 +1,14 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import './Checkout.css'
 import { Grid } from '@mui/material'
 import CheckoutItems from './CheckoutItems/CheckoutItems'
+import { CartContext } from '../CartContext'
 
 function Checkout() {
+    const {items} = useContext(CartContext)
+    let totalCartPrice = items.reduce((total, item) => 
+        total + parseInt(item.price.replaceAll(',','')), 0)
+
   return (
     <div className="checkout__main">
         <Grid container>
@@ -11,15 +16,17 @@ function Checkout() {
                 <div className="checkout__container">
                     <div style={{ fontSize:"30px", fontWeight:"500", padding:"20px 0 0 20px"}}>Shopping Cart</div>
                     <div>
-                        <CheckoutItems />
-                        <CheckoutItems />
-                        <CheckoutItems />
+                        {
+                            items.map((item, index) => 
+                                <CheckoutItems key={index} product={item} />
+                            )
+                        }
                     </div>
                 </div>
             </Grid>
             <Grid item xs={3}>
                 <div style={{width:"300px", height:"200px", marginTop:"25px", padding:"20px", backgroundColor:"white"}}>
-                    <div style={{ fontSize:"26px"}}>Subtotal (2 items): <strong>{'\u20B9'}1,01,998.00</strong></div>
+                    <div style={{ fontSize:"26px"}}>Subtotal ({items.length} items): <strong>{'\u20B9'}{totalCartPrice}</strong></div>
                     <div style={{ paddingTop:"25px" }}>
                         <button className="checkout__button">Proceed to Buy</button>
                     </div>
