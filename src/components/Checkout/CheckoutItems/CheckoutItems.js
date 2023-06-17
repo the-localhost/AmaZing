6adc24f41bfce352cfff36d2386c0cac7e6a2cbe
Gagan Agrawal <gaganagrawal.iit@gmail.com>
2023-06-17 16:17:58 +0530

    dynamic content in checkout items

--- src/components/Checkout/CheckoutItems/CheckoutItems.js
+++ src/components/Checkout/CheckoutItems/CheckoutItems.js
@@ -1,15 +1,15 @@
 import React from 'react'
 import './CheckoutItems.css'
 
-function CheckoutItems() {
+function CheckoutItems({product}) {
   return (
     <div style={{ border: "1px solid #E7E7E7", display:"flex",  width:"95%", height:"250px", margin:"25px" }}>
         <div style={{ margin: "25px" }}>
-            <img src="https://ik.imagekit.io/gaganEcomm/amazon-image/mobiles/71w3oJ7aWyL._AC_UL640_FMwebp_QL65_.webp?updatedAt=1681641021702" alt="product" height="200px"/>
+            <img src={product.image} alt={product.name} height="200px"/>
         </div>
         <div style={{ marginTop:"20px"}}>
-            <div className="textgap" style={{ fontSize:"20px" }}>New Apple iPhone 12 (64GB) - Green</div>
-            <div className="textgap" style={{ fontWeight:"bold" }}>{'\u20B9'}50,999</div>
+            <div className="textgap" style={{ fontSize:"20px" }}>{product.name}</div>
+            <div className="textgap" style={{ fontWeight:"bold" }}>{'\u20B9'}{product.price}</div>
             <div className="textgap">In Stock</div>
         </div>
     </div>
