30fd5789060c836c56ff9ff6e0cb8718b6c62f59
Gagan Agrawal <gaganagrawal.iit@gmail.com>
2023-06-17 13:03:00 +0530

    Added diff components with Route

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import './App.css';
+import Checkout from './components/Checkout/Checkout';
+import DisplayContent from './components/DisplayContent/DisplayContent';
+import MainPage from './components/MainPage/MainPage';
+import NavBar from './components/NavBar/Navigation';
+import PlaceOrder from './components/PlaceOrder/PlaceOrder';
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      
+      <NavBar />
+      <Routes>
+          <Route path="/display" element={<DisplayContent/>} />
+          <Route path="/order" element={<PlaceOrder />} />
+          <Route path="/checkout" element={<Checkout />} />
+          <Route path="/" element={<MainPage />} />
+      </Routes>
     </div>
   );
 }
