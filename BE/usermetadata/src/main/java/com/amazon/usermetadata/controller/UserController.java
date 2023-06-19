1432a86fdc79bb63a8dd5777f9d0f1b55737a4a9
Gagan Agrawal <gaganagrawal.iit@gmail.com>
2023-06-20 00:13:03 +0530

    using service for business logic

--- BE/usermetadata/src/main/java/com/amazon/usermetadata/controller/UserController.java
+++ BE/usermetadata/src/main/java/com/amazon/usermetadata/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.amazon.usermetadata.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -8,19 +9,25 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.amazon.usermetadata.entity.UserMetaData;
+import com.amazon.usermetadata.service.UserService;
 
 @RestController
 @RequestMapping("/amazon/user")
 public class UserController {
 	
+	@Autowired
+	private UserService userService;
+	
 	@PostMapping("saveUserDetails")
 	public UserMetaData saveUserDetails(@RequestBody UserMetaData userData) {
-		return new UserMetaData();
+		UserMetaData user = userService.saveUserMetaDataToDB(userData);
+		return user;
 	}
 	
 	@GetMapping("getUserDetails/{userId}")
 	public UserMetaData getUserDetails(@PathVariable String userId) {
-		return new UserMetaData();
+		UserMetaData user = userService.getUserMetaDataFromDB(userId);
+		return user;
 	}
 	
 }
