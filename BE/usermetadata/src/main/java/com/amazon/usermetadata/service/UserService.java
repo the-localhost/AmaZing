f46e61117c9a4ff6f361ef24b9321b151402881c
Gagan Agrawal <gaganagrawal.iit@gmail.com>
2023-06-20 00:14:22 +0530

    created user service to handle business logic

--- BE/usermetadata/src/main/java/com/amazon/usermetadata/service/UserService.java
+++ BE/usermetadata/src/main/java/com/amazon/usermetadata/service/UserService.java
@@ -1,5 +1,24 @@
 package com.amazon.usermetadata.service;
 
-public class UserService {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import com.amazon.usermetadata.entity.UserMetaData;
+import com.amazon.usermetadata.repository.UserMetaDataRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	UserMetaDataRepository userMetaDataRepository;
+	
+	// saving the data into the database
+	public UserMetaData saveUserMetaDataToDB(UserMetaData userMetaData) {
+		return userMetaDataRepository.save(userMetaData);
+	}
+	
+	// retrieve the data from the database for a user ID
+	public UserMetaData getUserMetaDataFromDB(String userId) {
+		return userMetaDataRepository.findByUniqueId(userId);
+	}
 }
