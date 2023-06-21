a66b404b98555d1d1731423741d0d517a8367b53
Gagan Agrawal <gaganagrawal.iit@gmail.com>
2023-06-21 12:28:00 +0530

    Excluded datasource autoconfig to be able to use mongodb

--- BE/productdetailsservice/src/main/java/com/amazon/productdetailsservice/ProductdetailsserviceApplication.java
+++ BE/productdetailsservice/src/main/java/com/amazon/productdetailsservice/ProductdetailsserviceApplication.java
@@ -2,8 +2,9 @@ package com.amazon.productdetailsservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class ProductdetailsserviceApplication {
 
 	public static void main(String[] args) {
