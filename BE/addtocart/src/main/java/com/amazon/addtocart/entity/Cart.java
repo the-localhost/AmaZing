8743abeeadc0643e5aa158bd478b4b70606703e5
Gagan Agrawal <gaganagrawal.iit@gmail.com>
2023-06-22 12:30:33 +0530

    Cart Entity

---
+++ BE/addtocart/src/main/java/com/amazon/addtocart/entity/Cart.java
@@ -0,0 +1,53 @@
+package com.amazon.addtocart.entity;
+
+import java.util.UUID;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
+public class Cart {
+	@Id
+	@GeneratedValue
+	public long id;
+
+	public UUID productId;
+	public UUID userId;
+
+	public Cart() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Cart(long id, UUID productId, UUID userId) {
+		super();
+		this.id = id;
+		this.productId = productId;
+		this.userId = userId;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public UUID getProductId() {
+		return productId;
+	}
+
+	public void setProductId(UUID productId) {
+		this.productId = productId;
+	}
+
+	public UUID getUserId() {
+		return userId;
+	}
+
+	public void setUserId(UUID userId) {
+		this.userId = userId;
+	}
+}
