d35ecd6e52663cef6abaf40514bc4a027400719a
Gagan Agrawal <gaganagrawal.iit@gmail.com>
2023-06-23 18:15:50 +0530

    corrected field name for image

--- FE/src/components/Checkout/CheckoutItems/CheckoutItems.js
+++ FE/src/components/Checkout/CheckoutItems/CheckoutItems.js
@@ -5,7 +5,7 @@ function CheckoutItems({product}) {
   return (
     <div style={{ border: "1px solid #E7E7E7", display:"flex",  width:"95%", height:"250px", margin:"25px" }}>
         <div style={{ margin: "25px" }}>
-            <img src={product.image} alt={product.name} height="200px"/>
+            <img src={product.imageURL} alt={product.name} height="200px"/>
         </div>
         <div style={{ marginTop:"20px"}}>
             <div className="textgap" style={{ fontSize:"20px" }}>{product.name}</div>
