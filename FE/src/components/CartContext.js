a5c19c88498ff1eeff15baf9ff2024f2a8b545de
Gagan Agrawal <gaganagrawal.iit@gmail.com>
2023-06-23 19:12:55 +0530

    getting cart size from backend

--- FE/src/components/CartContext.js
+++ FE/src/components/CartContext.js
@@ -6,8 +6,27 @@ export default class CartContextProvider extends Component {
     constructor(props) {
         super(props)
 
-        let existingCart = localStorage.getItem('myCart')
-        existingCart = (existingCart) ? JSON.parse(existingCart) : []
+        let existingCart = JSON.parse(localStorage.getItem('myCart'))
+        // console.log("ExistingCartOld:  ", existingCart)
+
+
+        const addToCart = async () => {
+          try{
+            const response = await fetch("http://localhost:8081/amazon/add-to-cart/show/962f9a88-426c-4c33-b50c-486dd4b8d6fe")
+            const cartItems = await response.json()
+            existingCart = cartItems.list
+          } catch (error) {
+              console.log("ERROR: ", error)
+          } finally {
+          }
+        }
+
+        addToCart()
+        existingCart = (existingCart) ? existingCart : []
+
+        // existingCart = addToCart().then(res => {return res})
+        // let existingCart = localStorage.getItem('myCart')
+        // existingCart = (existingCart) ? JSON.parse(existingCart) : addToCart().then(r => {return r})
         let existingCartSize = existingCart.length
 
         this.state = {
