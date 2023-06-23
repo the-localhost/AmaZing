e15389ce0b8827010ff05fa1ff28463c29c703d4
Gagan Agrawal <gaganagrawal.iit@gmail.com>
2023-06-23 16:05:01 +0530

    enabled feign client

--- BE/addtocart/src/main/java/com/amazon/addtocart/AddtocartApplication.java
+++ BE/addtocart/src/main/java/com/amazon/addtocart/AddtocartApplication.java
@@ -2,8 +2,10 @@ package com.amazon.addtocart;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class AddtocartApplication {
 
 	public static void main(String[] args) {
