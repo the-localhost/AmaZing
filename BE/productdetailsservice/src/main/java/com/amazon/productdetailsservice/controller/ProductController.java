b38c5865231233091505ed75a389b54dbecc518d
Gagan Agrawal <gaganagrawal.iit@gmail.com>
2023-06-23 16:47:37 +0530

    allowed cross origin

--- BE/productdetailsservice/src/main/java/com/amazon/productdetailsservice/controller/ProductController.java
+++ BE/productdetailsservice/src/main/java/com/amazon/productdetailsservice/controller/ProductController.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,6 +18,7 @@ import com.amazon.productdetailsservice.service.ProductService;
 
 @RestController
 @RequestMapping("/amazon/products")
+@CrossOrigin(origins="http://localhost:3000/")
 public class ProductController {
 	
 	@Autowired
