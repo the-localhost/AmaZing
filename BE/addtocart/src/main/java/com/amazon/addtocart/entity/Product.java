46704beea2910c5e81502802bf3b3099907855ac
Gagan Agrawal <gaganagrawal.iit@gmail.com>
2023-06-23 16:06:12 +0530

    product

---
+++ BE/addtocart/src/main/java/com/amazon/addtocart/entity/Product.java
@@ -0,0 +1,18 @@
+package com.amazon.addtocart.entity;
+
+import java.math.BigInteger;
+import java.util.UUID;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class Product {
+	public BigInteger id;
+
+	public UUID productId;
+	public String name;
+	public double price;
+	public double rating;
+	public String imageURL;
+}
