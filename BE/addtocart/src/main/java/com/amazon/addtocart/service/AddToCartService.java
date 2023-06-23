6dc01ea525e00235788466fa21ef37c7de05032a
Gagan Agrawal <gaganagrawal.iit@gmail.com>
2023-06-23 16:06:44 +0530

    using proxy to get details from productdetails microservice

--- BE/addtocart/src/main/java/com/amazon/addtocart/service/AddToCartService.java
+++ BE/addtocart/src/main/java/com/amazon/addtocart/service/AddToCartService.java
@@ -1,27 +1,46 @@
 package com.amazon.addtocart.service;
 
+import java.util.ArrayList;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.amazon.addtocart.entity.Cart;
+import com.amazon.addtocart.entity.CartDetails;
+import com.amazon.addtocart.entity.Product;
 import com.amazon.addtocart.repository.AddToCartRepository;
 
 @Service
 public class AddToCartService {
 	@Autowired
 	public AddToCartRepository addToCartRepository;
-	
+
+	@Autowired
+	public ProductDetailsProxy productDetailsProxy;
+
 	public void addToCart(Cart cart) {
 		addToCartRepository.save(cart);
 	}
-	
+
 	public void deleteFromCart(Cart cart) {
 		addToCartRepository.delete(cart);
 	}
-	
-	public void displayAllCartItemsForUser(UUID userId) {
-		addToCartRepository.findByUserId(userId);
+
+	public CartDetails displayAllCartItemsForUser(UUID userId) {
+		
+		CartDetails cartDetails = new CartDetails();
+		cartDetails.setUserId(userId);
+
+		ArrayList<Cart> cartItems = addToCartRepository.findByUserId(userId);
+		ArrayList<Product> products = new ArrayList<>();
+		for(Cart cartItem: cartItems) {
+			Product product = productDetailsProxy.getProductById(cartItem.getProductId());
+			products.add(product);
+		}
+		
+		cartDetails.setList(products);
+		
+		return cartDetails;
 	}
 }
