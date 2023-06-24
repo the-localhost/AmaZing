00b1dd5fa107d7e820657be86f81eebe68151fd0
Gagan Agrawal <gaganagrawal.iit@gmail.com>
2023-06-24 18:43:34 +0530

    added deleteItemFromCart functionality

--- FE/src/components/CartContext.js
+++ FE/src/components/CartContext.js
@@ -7,26 +7,24 @@ export default class CartContextProvider extends Component {
         super(props)
 
         let existingCart = JSON.parse(localStorage.getItem('myCart'))
-        // console.log("ExistingCartOld:  ", existingCart)
 
 
-        const addToCart = async () => {
+        const getCart = async () => {
           try{
             const response = await fetch("http://localhost:8081/amazon/add-to-cart/show/962f9a88-426c-4c33-b50c-486dd4b8d6fe")
             const cartItems = await response.json()
             existingCart = cartItems.list
+            localStorage.setItem('myCart', JSON.stringify(existingCart))
+            // console.log("EXCart", existingCart)
           } catch (error) {
               console.log("ERROR: ", error)
           } finally {
           }
-        }
+        }        
 
-        addToCart()
+        getCart()
         existingCart = (existingCart) ? existingCart : []
 
-        // existingCart = addToCart().then(res => {return res})
-        // let existingCart = localStorage.getItem('myCart')
-        // existingCart = (existingCart) ? JSON.parse(existingCart) : addToCart().then(r => {return r})
         let existingCartSize = existingCart.length
 
         this.state = {
@@ -41,6 +39,25 @@ export default class CartContextProvider extends Component {
             },
             saveToLocalCache: () => {
                 localStorage.setItem('myCart', JSON.stringify(this.state.items))
+            },
+            deleteItemFromCart: async (product) => {
+              console.log("Product ID: ", product.productId, "\nBefore delete cart:  ", this.state.items)
+              try {
+                await fetch("http://localhost:8081/amazon/add-to-cart/delete",{
+                  method: "DELETE",
+                  body: JSON.stringify({
+                    "userId": "962f9a88-426c-4c33-b50c-486dd4b8d6fe",
+                    "productId": product.productId
+                  }),
+                  headers: {
+                      "Content-type": "application/json; charset=UTF-8"
+                  }
+                })
+                await getCart()
+                this.setState({...this.state, items: existingCart, size: existingCart.length})
+              } catch (error) {
+                console.log("ERROR: ", error)
+              }
             }
         }
     }
